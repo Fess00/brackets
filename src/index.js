2f251b1dfffc95f21287ac6be4fa80e2c49df327
Fess00 <ss_s0l0@mail.ru>
2021-03-15 20:54:56 +0300

    completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let config = new Map(bracketsConfig);
+    let right = [];
+    let check = [];
+    for (let i of str.split('')) {
+        if (config.has(i)) {
+            check.push(i);
+            right.push(config.get(i));
+        }
+    }
+    right.reverse();
+    for (let i = 0; i < right.length; i++) {
+        if (check[i] !== right[i]) {
+            return false;
+        }
+    }
+    return check.length === right.length;
 }
